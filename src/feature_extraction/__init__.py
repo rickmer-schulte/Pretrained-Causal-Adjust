d816cd7f27af0753cdc9a88e88781c06cdbe56c4
rickmer-schulte <kiwirickmer@gmail.com>
2025-06-16 19:27:59 +0200

    :wrench: minor fix of init

--- src/feature_extraction/__init__.py
+++ src/feature_extraction/__init__.py
@@ -1,4 +1,4 @@
 # feature_extraction/__init__.py
 
-from .pretrained_models import load_pretrained_resnet50, extract_features, preprocess_and_extract_features
 from .preprocess import get_preprocess_pipeline
+from .pretrained_models_xrv import load_torchxrayvision_model, extract_features_from_folder
